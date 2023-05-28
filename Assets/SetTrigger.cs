501590642fa51242ff240a3e49660f567084cf4a
ksemed <71792898+ksemed@users.noreply.github.com>
2023-05-28 03:35:45 -0700

    Changed box colliders for some items so they can move together
    Explosion is still a little weird and I'm still working through how I can make the UI pop-up only after the explosion occurs.

---
+++ Assets/SetTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] ObjectCollider = GetComponents<Collider>();
+        GameObject confirm = GameObject.Find("Exploding Confirmation");
+/*
+        if(confirm) {
+            ObjectCollider[1].isTrigger = false;
+        } 
+        else if (!confirm) {
+            ObjectCollider[1].isTrigger = true;
+        }
+*/
+        ObjectCollider[1].isTrigger = true;       
+    }
+}
